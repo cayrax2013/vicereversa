e2476538870030fb90ff0ba1d2b39edfdcf16284
out-boger-va <out-boger-va>
2021-01-11 01:35:32 +0300

    Add count original text

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from collections import Counter
 
 def about(request):
 	return HttpResponse("This is about")
@@ -12,4 +13,5 @@ def home(request):
 def reverse(request):
 	user_text = request.GET['usertext']
 	reversed_text = user_text[::-1]
-	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text})
+	count_words = len(user_text.split())
+	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text, 'countwords':count_words})
