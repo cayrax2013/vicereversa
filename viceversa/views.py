7c33582850d15d82d120376e746015c45a97b135
out-boger-va <out-boger-va>
2021-01-11 00:44:36 +0300

    My first project with Django

---
+++ viceversa/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def about(request):
+	return HttpResponse("This is about")
+
+
+def home(request):
+	return render(request, 'home.html', {'greeting':'Hello!'})
