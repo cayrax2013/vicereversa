3ad0bf4cfaabc773dbfa6b2a8377ce5de25419a7
out-boger-va <out-boger-va>
2021-01-11 01:20:12 +0300

    Add reverse text future

--- viceversa/settings.py
+++ viceversa/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myfirstproject.urls'
+ROOT_URLCONF = 'viceversa.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myfirstproject.wsgi.application'
+WSGI_APPLICATION = 'viceversa.wsgi.application'
 
 
 # Database
--- viceversa/wsgi.py
+++ viceversa/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myfirstproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viceversa.settings')
 
 application = get_wsgi_application()
